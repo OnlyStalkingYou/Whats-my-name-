bf966612082a75c09af88f9d09c426db6a0750c8
Leon.U <onlystalkingyou@hotmail.com>
2018-10-14 01:28:24 +0000

    new dino form and link

--- app/controllers/dinos_controller.rb
+++ app/controllers/dinos_controller.rb
@@ -4,4 +4,19 @@ class DinosController < ApplicationController
 		@dinos = Dino.all
 	end
 
+	def new
+		@dino = Dino.new
+	end
+
+	def create
+    Dino.create(dino_params)
+    redirect_to root_path
+	end
+
+	private
+
+	def dino_params
+	params.require(:dino).permit(:name, :pronunciation, :era, :inhabited, :features, :description)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	root 'dinos#index'
+	resources :dinos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
